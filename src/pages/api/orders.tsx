21b4e42a7feb4f44e31298f5353e5c2e6ffc5496
tonnidiaz <tonnidiazed@gmail.com>
2024-05-24 13:51:17 +0200

    /api/orders

---
+++ src/pages/api/orders.tsx
@@ -0,0 +1,12 @@
+import { API } from "@/src/utils/constants";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    try{
+       const  _res = await API.get('/orders')
+        res.status(200).json(_res.data)
+    }catch(e){
+        console.log(e);
+        res.status(500).json({msg: 'Something went wrong'})
+    }
+}
