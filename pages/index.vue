4831e9ff2f3339f2ccac10678191fdaa0af7717d
clickbait-4587 <clickbait4587@gmail.com>
2024-06-19 06:11:26 +0200

    mrkt okx

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        <h1>Index page</h1>
+    <div className="w-100p h-100p flex flex-col items-center justify-center">
+        <TMeta/>
+        <h1 class="fs-45 fw-8">{{SITE}}</h1>
+        <p>A Trading Bot from <b>Tunedbass</b></p>
     </div>
 </template>
